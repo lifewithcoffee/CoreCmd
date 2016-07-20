b51dd031eb6c5416e1b9e182d7da659e5d186cc5
RL <li.rongcheng@gmail.com>
2016-07-20 16:28:56 +1000

    add a DefaultCommand, which will be used if the specified command is not available

--- src/CoreCmd/CommandExecutor.cs
+++ src/CoreCmd/CommandExecutor.cs
@@ -4,25 +4,50 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace CoreCmd.Commands
+namespace CoreCmd
 {
     public class CommandExecutor
     {
         internal void Execute(string[] args)
         {
-            if (ValidateArguments(args))
+            if (args.Length > 0)
             {
-                string command = args[0];
-                string method = args[1];
+                string command = "DefaultCommand";
+                string method;
+                string[] parameters;
 
-                string[] parameters = args.Skip(2).ToArray();
+                if (FindCommandOf(args[0]))
+                {
+                    command = args[0];
+                    method = args[1];
+                    parameters = args.Skip(2).ToArray();
+                }
+                else
+                {
+                    method = args[0];
+                    parameters = args.Skip(1).ToArray();
+                }
+
                 ExecuteCommand(command, method, parameters);
             }
             else
             {
-                throw new NotImplementedException();
+                Console.WriteLine("Subcommand is missing, please specify subcommands"); // [TODO]$0329: need to print all available subcommands here
             }
+        }
 
+        private bool FindCommandOf(string v)
+        {
+            const string defaultNamespace = "CoreCmd.Commands";
+            Type targetType = Assembly.GetEntryAssembly().GetTypes().Where(t => t.Namespace.Equals(defaultNamespace) && t.Name.Equals(v)).FirstOrDefault();
+            if(targetType != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private bool ValidateArguments(string[] args)
@@ -34,60 +59,47 @@ namespace CoreCmd.Commands
         {
             try
             {
-                if (FindCommand(command))
+                string fullTypeWithNamespace = string.Format("{0}.Commands.{1}", typeof(Program).Namespace, command);
+                Type commandType = Type.GetType(fullTypeWithNamespace);
+                var ins = Activator.CreateInstance(commandType);
+
+                object[] paramObjs = new object[parameters.Length];
+                ParameterInfo[] paramInfo = commandType.GetMethod(method).GetParameters();
+
+                if (paramInfo.Length != parameters.Length)
                 {
-                    string fullTypeWithNamespace = string.Format("{0}.Commands.{1}", typeof(Program).Namespace, command);
-                    Type commandType = Type.GetType(fullTypeWithNamespace);
-                    var ins = Activator.CreateInstance(commandType);
-
-                    object[] paramObjs = new object[parameters.Length];
-                    ParameterInfo[] paramInfo = commandType.GetMethod(method).GetParameters();
-
-                    if (paramInfo.Length != parameters.Length)
-                    {
-                        throw new Exception(string.Format("Incorrect argument number, command {0}.{1} can accept {2} argument(s).", command, method, paramInfo.Length));
-                    }
-
-                    for(int i=0; i< paramInfo.Length; i++)
-                    {
-                        switch (Type.GetTypeCode(paramInfo[i].ParameterType))
-                        {
-                            case TypeCode.Byte:
-                            case TypeCode.SByte:
-                            case TypeCode.UInt16:
-                            case TypeCode.UInt32:
-                            case TypeCode.UInt64:
-                            case TypeCode.Int16:
-                            case TypeCode.Int32:
-                            case TypeCode.Int64:
-                            case TypeCode.Decimal:
-                            case TypeCode.Double:
-                            case TypeCode.Single:
-                                paramObjs[i] = int.Parse(parameters[i]);
-                                break;
-                            default:
-                                paramObjs[i] = parameters[i];
-                                break;
-                        }
-                    }
-
-                    commandType.GetMethod(method).Invoke(ins, paramObjs);
+                    throw new Exception(string.Format("Incorrect argument number, command {0}.{1} can accept {2} argument(s).", command, method, paramInfo.Length));
                 }
-                else
+
+                for(int i=0; i< paramInfo.Length; i++)
                 {
-                    Console.WriteLine("Command {0} not found", command);
+                    switch (Type.GetTypeCode(paramInfo[i].ParameterType))
+                    {
+                        case TypeCode.Byte:
+                        case TypeCode.SByte:
+                        case TypeCode.UInt16:
+                        case TypeCode.UInt32:
+                        case TypeCode.UInt64:
+                        case TypeCode.Int16:
+                        case TypeCode.Int32:
+                        case TypeCode.Int64:
+                        case TypeCode.Decimal:
+                        case TypeCode.Double:
+                        case TypeCode.Single:
+                            paramObjs[i] = int.Parse(parameters[i]);
+                            break;
+                        default:
+                            paramObjs[i] = parameters[i];
+                            break;
+                    }
                 }
+
+                commandType.GetMethod(method).Invoke(ins, paramObjs);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-        }
-
-        private bool FindCommand(string command)
-        {
-            Console.WriteLine("FindCommand: not implemented");
-            return true;
         }
     }
 }
