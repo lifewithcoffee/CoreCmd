dba367a2f759ca0332bc48f039ed6f1c5f19756d
Ron <li.rongcheng@gmail.com>
2018-07-13 01:04:06 +1000

    Add HelpCommand in CoreCmd and add a HelpAttribute for classes and methods

---
+++ CoreCmd/HelpCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCmd
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method )]
+    public class HelpAttribute : Attribute
+    {
+        public HelpAttribute(String description_in)
+        {
+            this.description = description_in;
+        }
+
+        protected String description;
+        public String Description { get { return this.description; } }
+    }
+
+    class HelpCommand
+    {
+        ICommandFinder _commandFinder = new CommandFinder();
+
+        public void Show(string command)
+        {
+            const string commandPostfix = "command";
+            var allClassTypes = _commandFinder.GetCommandClassTypes(commandPostfix);
+            var targetCommand = _commandFinder.GetTargetCommandObject(allClassTypes, new string[]{ command });
+
+            if(targetCommand != null)
+                targetCommand.PrintHelp();
+            else
+                Console.WriteLine("No command object found");
+        }
+    }
+}
---
+++ DependentConsoleApp/GoodMorningCommand.cs
@@ -0,0 +1,26 @@
+﻿using CoreCmd;
+using System;
+
+namespace DependentConsoleApp
+{
+    [Help("This is the help info of command " + nameof(GoodMorningCommand) + ".")]
+    public class GoodMorningCommand
+    {
+        public void DefaultMethod()
+        {
+            Console.WriteLine("GreetingCommand.DefaultMethod() called");
+        }
+
+        [Help("This is the help info of method " + nameof(SayHello) + ".")]
+        public void SayHello()
+        {
+            Console.WriteLine("GreetingCommand.SayHello() called");
+        }
+
+        [Help("This is the help info of method " + nameof(Hello) + ".")]
+        public void Hello(string param1, double param2)
+        {
+            Console.WriteLine($"GreetingCommand.Hello({param1},{param2}) called");
+        }
+    }
+}
