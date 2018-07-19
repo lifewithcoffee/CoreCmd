f2aca5a22dfbe14e00617853c7e56b5e10d8695e
Ron <li.rongcheng@gmail.com>
2018-07-19 18:50:09 +1000

    refactoring: move HelpAttribute to a separate file

---
+++ CoreCmd/BuiltinCommands/HelpAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCmd.BuiltinCommands
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class HelpAttribute : Attribute
+    {
+        public HelpAttribute(String description_in)
+        {
+            this.description = description_in;
+        }
+
+        protected String description;
+        public String Description { get { return this.description; } }
+    }
+}
--- CoreCmd/BuiltinCommands/HelpCommand.cs
+++ CoreCmd/BuiltinCommands/HelpCommand.cs
@@ -5,18 +5,6 @@ using System.Text;
 
 namespace CoreCmd.BuiltinCommands
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method )]
-    public class HelpAttribute : Attribute
-    {
-        public HelpAttribute(String description_in)
-        {
-            this.description = description_in;
-        }
-
-        protected String description;
-        public String Description { get { return this.description; } }
-    }
-
     class HelpCommand
     {
         ICommandFinder _commandFinder = new CommandFinder();
