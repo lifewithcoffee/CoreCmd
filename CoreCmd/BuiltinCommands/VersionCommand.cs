335c76b714d2c3a8ec0319dcb6845bda23524f31
Ron <li.rongcheng@gmail.com>
2018-07-22 23:31:56 +1000

    add a new build-in command VersionCommand

---
+++ CoreCmd/BuiltinCommands/VersionCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace CoreCmd.BuiltinCommands
+{
+    class VersionCommand
+    {
+        public void DefaultMethod()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string version= FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+
+            Console.WriteLine(assembly.Location);
+            Console.WriteLine(version);
+        }
+    }
+}
