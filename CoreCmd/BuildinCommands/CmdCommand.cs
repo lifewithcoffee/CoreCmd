20d87ea9bc7e8c541228712e680a4ef4d6296166
Ron <li.rongcheng@gmail.com>
2018-08-01 15:26:19 +1000

    add CmdCommand buildin subcommand

---
+++ CoreCmd/BuildinCommands/CmdCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCmd.BuildinCommands
+{
+    class CmdCommand
+    {
+        public void DefaultMethod()
+        {
+            this.List();
+        }
+
+        public void Add(string dllPath, bool local=false)
+        {
+            var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Console.WriteLine($"userDir: {userDir}");
+        }
+
+        public void List()
+        {
+            Console.WriteLine("Cmd.List() called");
+        }
+
+        public void Remove(int[] dllIds)
+        {
+            Console.WriteLine("Cmd.Remove() called");
+        }
+
+        public void Disable(int[] dllIds)
+        {
+            Console.WriteLine("Cmd.Disable() called");
+        }
+    }
+}
